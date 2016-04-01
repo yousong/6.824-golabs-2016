02c1c250164e00fd3e3ab01da1b9d720fd6ff0bd
Yousong Zhou <yszhou4tech@gmail.com>
2016-04-01 16:24:52 +0800

    mapreduce: Distributing MapReduce tasks
    If there were "too many open files" message in the output of "go test
    -run TestBasic -v mapreduce", first check whether all files are closed
    with defer in doMap and doReduce.  Otherwise just increase the limit
    with
    
        ulimit -n 1024

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -1,6 +1,10 @@
 package mapreduce
 
-import "fmt"
+import (
+	"fmt"
+)
+
+var cworker chan string
 
 // schedule starts and waits for all tasks in the given phase (Map or Reduce).
 func (mr *Master) schedule(phase jobPhase) {
@@ -17,12 +21,65 @@ func (mr *Master) schedule(phase jobPhase) {
 
 	fmt.Printf("Schedule: %v %v tasks (%d I/Os)\n", ntasks, phase, nios)
 
-	// All ntasks tasks have to be scheduled on workers, and only once all of
-	// them have been completed successfully should the function return.
-	// Remember that workers may fail, and that any given worker may finish
-	// multiple tasks.
-	//
-	// TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
-	//
+	ctask := make(chan int, ntasks)
+	cdone := make(chan bool, 4)
+	if cworker == nil {
+		cworker = make(chan string, nios)
+		go func() {
+			for {
+				w := <-mr.registerChannel
+				cworker <- w
+			}
+		}()
+	}
+	go func() {
+		for i := 0; i < ntasks; i++ {
+			ctask <- i
+		}
+	}()
+
+	go func() {
+		for {
+			t, ok := <-ctask
+			if ok == false {
+				return
+			}
+			w := <-cworker
+
+			go func() {
+				var res bool
+				args := new(DoTaskArgs)
+				args.JobName = mr.jobName
+				args.TaskNumber = t
+				args.NumOtherPhase = nios
+				args.Phase = phase
+				switch phase {
+				case mapPhase:
+					args.File = mr.files[t]
+					res = call(w, "Worker.DoTask", args, new(struct{}))
+				case reducePhase:
+					res = call(w, "Worker.DoTask", args, new(struct{}))
+				}
+				cworker <- w
+				if res == false {
+					ctask <- t
+				}
+				cdone <- res
+			}()
+		}
+	}()
+
+	var doneCnt = 0
+	for {
+		res := <-cdone
+		if res == true {
+			doneCnt += 1
+			if doneCnt == ntasks {
+				close(ctask)
+				break
+			}
+		}
+	}
+
 	fmt.Printf("Schedule: %v phase done\n", phase)
 }
